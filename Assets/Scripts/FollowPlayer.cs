34dcdb38ccc5cc0fe415f7ffbf8153d274538458
nathan241 <98729291+nathan241@users.noreply.github.com>
2022-02-13 19:38:18 -0800

    Unit1 Lession C

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -2,19 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 6, -12);
     // Start is called before the first frame update
-    public float speed = 15.0f;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-
+        transform.position = player.transform.position + offset;
     }
 }
